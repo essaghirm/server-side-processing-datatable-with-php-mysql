7e9ad07da7551a60377479a0873ac3f71556fcfa
essaghirm <34938901+essaghirm@users.noreply.github.com>
2020-02-14 11:44:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Server Side Processing DataTable with PHP and MySQL - Learn infinity</title>
+    
+    <!-- Bootstrap Core Css -->
+    <link href="css/bootstrap.css" rel="stylesheet" />
+
+    <!-- Font Awesome Css -->
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+
+	<!-- Bootstrap Select Css -->
+    <link href="css/bootstrap-select.css" rel="stylesheet" />
+
+    <!-- Custom Css -->
+    <link href="css/app_style.css" rel="stylesheet" />
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<!-- DataTables CSS -->
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css"/>
+	
+	<style>
+	.cust-table {
+	    margin: 35px 0px;
+	}
+	.cust-table th {
+	    padding: 10px;
+	}
+	table.dataTable tr.odd {
+		background-color: #ace0e0;
+	}
+	table.dataTable tr.even td.sorting_1 {
+		background-color: #ffffff;
+	}
+	table.dataTable.hover tbody tr:hover, table.dataTable.display tbody tr:hover {
+		background-color: #ace0e0;
+	}
+	</style>
+</head>
+<body>
+    <div class="all-content-wrapper">
+		<!-- Top Bar -->
+		<?php require_once('./include/header.php'); ?>
+		<!-- #END# Top Bar -->
+	
+		<section class="container">
+			<div class="form-group custom-input-space has-feedback">
+				<div class="page-heading">
+					<h3 class="post-title">Server Side Processing DataTable with PHP and MySQL</h3>
+				</div>
+				<div class="page-body clearfix">
+					<div class="row">
+						<div class="col-md-offset-1 col-md-10">
+							<div class="panel panel-default">
+								<div class="panel-heading">Post Details List:</div>
+								<div class="panel-body">
+									
+									<table id="post_list" class="dataTable" width="100%" cellspacing="0">
+										<thead>
+											<tr>
+												<th>S.No.</th>
+												<th>Post Name</th>
+												<th>Description</th>
+											</tr>
+										</thead>
+								 
+									</table>
+									
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+    </div>
+	
+	<!-- Jquery Core Js -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core Js -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Bootstrap Select Js -->
+    <script src="js/bootstrap-select.js"></script>
+	
+	<!-- DataTables -->
+	<script type="text/javascript" src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
+	<script>
+	
+	$(document).ready(function(e){
+		$('#post_list').dataTable({
+			"bProcessing": true,
+         	"serverSide": true,
+         	"ajax":{
+	            url :"post_list.php",
+	            type: "POST",
+	            error: function(){
+	              $("#post_list_processing").css("display","none");
+	            }
+          	}
+        });
+	});
+
+
+    </script>
+</body>
+</html>
